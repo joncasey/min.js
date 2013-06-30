c73e7addcfd2eae8d00de0e3f3218a0c23e39a55
joncasey <joncasey@gmail.com>
2013-06-30 09:58:33 -0400

    Update $.js to include the optional delegate.
    There's a few interesting ideas in this.
    - Conditionally append, so it wouldn't overwrite $.
    - Check which "matches" method is available, if any?
    - Appends a simple "matches" if none exists.
    - Reduction of document references.

--- src/$.js
+++ src/$.js
@@ -1,51 +1,53 @@
-/*globals Node:true, NodeList:true*/
-$ = (function (document, window, $) {
-  var node = Node.prototype,
-      nodeList = NodeList.prototype,
-      forEach = 'forEach',
-      trigger = 'trigger',
-      each = [][forEach],
-      dummy = document.createElement();
+!this.$ && function(window) {
+  var node    = Node.prototype
+    , list    = NodeList.prototype
+    , matches = 'matches matchesSelector msMatchesSelector mozMatchesSelector oMatchesSelector webkitMatchesSelector'.split(' ')
+                .filter(function(fn) { return fn in document.documentElement })[0]
 
-  nodeList[forEach] = each;
+  node.on = window.on = function(e, fn) {
+    /// <signature><param name="e" type="String"/><param name="selector" type="String"/><param name="fn" type="Function"/></signature>
+    /// <signature><param name="e" type="String"/><param name="fn" type="Function"/></signature>
+    if (arguments.length == 3) {
+      var selector = fn ; fn = arguments[2]
+      return this.on(e, function(e) {
+        if (e.target[matches](selector)) {
+          fn.call(e.target, e)
+        }
+      })
+    }
+    this.addEventListener(e, fn)
+    return this
+  }
 
-  window.on = node.on = function (event, fn) {
-    this.addEventListener(event, fn, false);
-    return this;
-  };
+  node.trigger = window.trigger = function(type, data) {
+    var e = window.document.createEvent('HTMLEvents')
+    e.initEvent(type, true, true)
+    e.data = data || {}
+    e.eventName = type
+    e.target = this
+    this.dispatchEvent(e)
+    return this
+  }
 
-  nodeList.on = function (event, fn) {
-    each.call(this, function (el) {
-      el.on(event, fn);
-    });
-    return this;
-  };
+  list.forEach = Array.prototype.forEach
+  list.on = function(e, fn) {
+    this.forEach(function(el) { el.on(e, fn) })
+    return this
+  }
 
-  window[trigger] = node[trigger] = function (type, data) {
-    var event = document.createEvent('HTMLEvents');
-    event.initEvent(type, true, true);
-    event.data = data || {};
-    event.eventName = type;
-    event.target = this;
-    this.dispatchEvent(event);
-    return this;
-  };
+  list.trigger = function(e) {
+    this.forEach(function(el) { el.trigger(e) })
+    return this
+  }
 
-  nodeList[trigger] = function (event) {
-    each.call(this, function (el) {
-      el[trigger](event);
-    });
-    return this;
-  };
+  window.$ = function(selector) {
+    return this.document.querySelectorAll(selector)
+  }
 
-  $ = function (s) {
-    var r = document.querySelectorAll(s || '☺'),
-        length = r.length;
-    return length == 1 ? r[0] : !length ? nodeList : r;
-  };
+  if (!matches) {
+    node[matches = 'matches'] = function(selector) {
+      return Array.prototype.indexOf.call($(selector), this) > -1
+    }
+  }
 
-  $.on = node.on.bind(dummy);
-  $[trigger] = node[trigger].bind(dummy);
-
-  return $;
-})(document, this);
+}(this)
